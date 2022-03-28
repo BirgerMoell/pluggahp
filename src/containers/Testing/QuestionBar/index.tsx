8ba7fcc9ec2b6d830fb4431dfd4dc44c1526024b
Patrick O'Regan <patrick.oregan@pricerunner.com>
2022-03-28 08:32:17 +0200

    wip: new test page

--- src/containers/Testing/QuestionBar/index.tsx
+++ src/containers/Testing/QuestionBar/index.tsx
@@ -1,5 +1,8 @@
-import { Box, Button, Toolbar } from "@mui/material";
+import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
+import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
+import { Box, Button, MenuItem, Select, Stack, Toolbar } from "@mui/material";
 import { FC } from "react";
+import { COLORS } from "../../../constants/colors";
 import segments, { Solution } from "../../../data/segments";
 import { useCurrentQuestion } from "../../../providers/CurrentQuestionProvider";
 
@@ -9,12 +12,13 @@ type Props = {
 
 const QuestionBar: FC<Props> = ({ registerAnswer }) => {
   const { currentQuestion } = useCurrentQuestion();
+
   if (!currentQuestion) {
     return null;
   }
   const segment = segments[currentQuestion.segment];
   return (
-    <Box
+    <Stack
       sx={{
         backgroundColor: "#fff",
         borderTop: "1px solid #dedede",
@@ -52,7 +56,73 @@ const QuestionBar: FC<Props> = ({ registerAnswer }) => {
           ))}
         </div>
       </Toolbar>
-    </Box>
+      <Box
+        sx={{
+          color: COLORS.textPrimary,
+          fontSize: "18px",
+          borderTop: "1px solid #dedede",
+          width: "100%",
+          minHeight: "38px",
+          alignItems: "center",
+          display: "flex",
+        }}
+      >
+        <div
+          style={{
+            textAlign: "center",
+            width: "75px",
+            borderRight: "1px solid #dedede",
+          }}
+        >
+          <ArrowBackIosOutlinedIcon />
+        </div>
+        <div
+          style={{
+            textAlign: "center",
+            flexGrow: 1,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Select
+            sx={{
+              maxHeight: "38px",
+              border: "0",
+              "> fieldset": {
+                border: "0",
+              },
+            }}
+            MenuProps={{
+              MenuListProps: {
+                sx: {
+                  minWidth: "170px",
+                  maxHeight: "500px",
+                },
+              },
+            }}
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={0}
+            label="Age"
+            onChange={(value) => console.log(value)}
+          >
+            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map((i) => (
+              <MenuItem value={i}>Uppgift {i + 1}</MenuItem>
+            ))}
+          </Select>
+        </div>
+        <div
+          style={{
+            textAlign: "center",
+            width: "75px",
+            borderLeft: "1px solid #dedede",
+          }}
+        >
+          <ArrowForwardIosOutlinedIcon />
+        </div>
+      </Box>
+    </Stack>
   );
 };
 
